97fec5f706b39192976f71dc27975417de0e2553
erangeljr <hacker@eddierangel.com>
2016-04-03 21:04:36 -0700

    Stack Implementation

--- stack.cpp
+++ stack.cpp
@@ -1,18 +1,11 @@
 /*	Eddie Rangel					*/
-/*	COMS B35 OOP C++				*/
-/*	CH4-B.CPP						*/
-/*	2-13-03							*/
-/*	Asks user for a double			*/
-/*	HEX value in each byte			*/
 
-
-#include<iostream.h>
-#include<conio.h>
-#include<ctype.h>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
 
 const int max_len = 5;
 enum {EMPTY = -1, FULL = max_len - 1};
-enum boolean{false,true}; 			/* This is for B.C. commons only */
 
 class stack
 {
@@ -21,8 +14,8 @@ class stack
 		void push(double, stack &stk);
 		double pop(stack &stk);
 		double peek(const stack &stk);
-		boolean full(const stack &stk);
-		boolean empty(const stack &stk);
+		bool full(const stack &stk);
+		bool empty(const stack &stk);
 	private:
 		double d[max_len];
 		int top;
@@ -33,7 +26,6 @@ main()
 	double nmbr;
 	char c;
 	stack mystack;
-	clrscr();
 	mystack.reset(mystack);
 	cout << "Please enter +, -, P, C, or Q: ";
 	cin >> c;
@@ -100,13 +92,13 @@ double stack::peek(const stack &stk)
 {
 	return stk.d[stk.top];
 }
-boolean stack::full(const stack &stk)
+bool stack::full(const stack &stk)
 {
-	return (boolean)(stk.top == FULL);
+	return (bool)(stk.top == FULL);
 }
-boolean stack::empty(const stack &stk)
+bool stack::empty(const stack &stk)
 {
-	return (boolean)(stk.top == EMPTY);
+	return (bool)(stk.top == EMPTY);
 }
 
 
